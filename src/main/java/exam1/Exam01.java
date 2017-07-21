ccf1069706b9f4354c718d87b8da92dff4e230e1
Preedaphong Rattanaprach <preedaphong.r@kasikornbank.com>
2017-07-21 15:18:48 +0700

    passed two test cases (can check player A win)

--- src/main/java/exam1/Exam01.java
+++ src/main/java/exam1/Exam01.java
@@ -2,18 +2,28 @@ package exam1;
 
 public class Exam01 {
 
-    public Exam01(String firstPerson, String secondPerson) {
+    boolean A_win ;
+    boolean B_win ;
 
+    public Exam01(String firstPerson, String secondPerson) {
+        A_win = false;
+        B_win = false;
     }
 
 
     public void playerAWin() {
+        A_win = true;
     }
 
     public void playerBWin() {
+        B_win = true;
     }
 
     public String getScore() {
+
+        if(A_win){
+            return "Fifteen-Love";
+        }
         return "Love-All";
     }
 
